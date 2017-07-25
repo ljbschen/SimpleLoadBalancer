c35423d483215f1b3ab831de042d0e693f6f06b2
kevinchen <ljbschen@gmail.com>
2017-07-24 21:47:33 -0700

    update round robin

--- LoadBalancer/src/main/java/balancer/FrameByFrameServiceConfiguration.java
+++ LoadBalancer/src/main/java/balancer/FrameByFrameServiceConfiguration.java
@@ -1,10 +1,7 @@
 package balancer;
 
 import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.AvailabilityFilteringRule;
-import com.netflix.loadbalancer.IPing;
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.PingUrl;
+import com.netflix.loadbalancer.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
@@ -19,6 +16,7 @@ public class FrameByFrameServiceConfiguration {
 
     @Bean
     public IRule ribbonRule(IClientConfig config) {
-        return new AvailabilityFilteringRule();
+        return new RoundRobinRule();
+//        return new AvailabilityFilteringRule();
     }
 }
