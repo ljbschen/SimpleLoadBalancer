eab729a852dc19887a3341b6e7fabbcdd8d35711
kevinchen <ljbschen@gmail.com>
2017-07-24 19:19:43 -0700

    first commit

---
+++ Service/src/main/java/app/ServiceApplication.java
@@ -0,0 +1,38 @@
+package app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@SpringBootApplication
+public class ServiceApplication {
+    private static Logger log = LoggerFactory.getLogger(ServiceApplication.class);
+
+    @RequestMapping(value = "/greeting")
+    public String greet() {
+        log.info("Access /greeting");
+        List<String> greetings = Arrays.asList("Hi there", "Greetings", "Salutations");
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt(greetings.size());
+        return greetings.get(randomNum) + "2";
+    }
+
+    @RequestMapping(value = "/")
+    public String home() {
+        log.info("Access /");
+        return "Hi!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
